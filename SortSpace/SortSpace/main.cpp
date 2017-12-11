2e94fd7f6c75f585a89cedc9cf0bf2c3c9fdedb5
JamesChenGithub <403725592@qq.com>
2017-12-11 18:04:19 +0800

    改成main.cpp

--- SortSpace/SortSpace/main.cpp
+++ SortSpace/SortSpace/main.cpp
@@ -6,12 +6,10 @@
 //  Copyright © 2017年 AlexiChen. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#include <iostream>
+#include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+   
     return 0;
 }
