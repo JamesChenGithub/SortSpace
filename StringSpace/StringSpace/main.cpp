4eb100ca2c567a260c558ac476bee674786658f3
JamesChenGithub <403725592@qq.com>
2017-12-21 12:26:58 +0800

    add bad map ;  add sunday search

--- StringSpace/StringSpace/main.cpp
+++ StringSpace/StringSpace/main.cpp
@@ -10,6 +10,7 @@
 #include <string>
 #include <stdio.h>
 #include <stdlib.h>
+#include <map>
 
 using namespace std;
 
@@ -93,7 +94,10 @@ public:
             cout << "第"<< ++count << "次比较： patternCPtr[" << p_index-1 << "] (" << patternCPtr[p_index-1] <<") != matchCPtr[" << s_index-1 << "] ("<< matchStr[s_index-1] <<")" << endl;
             while (matchStr[--s_index] == patternCPtr[--p_index])
             {
-                cout << "第"<< ++count << "次比较： patternCPtr[" << p_index << "] (" << patternCPtr[p_index] <<") != matchCPtr[" << s_index << "] ("<< matchStr[s_index] <<")" << endl;
+                if (p_index != (patternLen - 1))
+                {
+                    cout << "第"<< ++count << "次比较： patternCPtr[" << p_index << "] (" << patternCPtr[p_index] <<") != matchCPtr[" << s_index << "] ("<< matchStr[s_index] <<")" << endl;
+                }
                 if (p_index == 0)
                 {
                     return s_index;
@@ -250,7 +254,309 @@ public:
 
     
     // BM算法
+    // 坏字符匹配
+    /* 函数：void buildBadC(char *, int, int*)
+     * 目的：根据好后缀规则做预处理，建立一张好后缀表
+     * 参数：
+     * pattern => 模式串P
+     * plen => 模式串P长度
+     * shift => 存放坏字符规则表,长度为的int数组
+     */
+    void buildBadC(const string ps, unsigned char *badShift = nullptr, int shiftlength = 256)
+    {
+        if (badShift)
+        {
+            
+            int psLen = (int)ps.length();
+            
+            memset(badShift, psLen, sizeof(unsigned char) * shiftlength);
+            
+            const char *psPtr = ps.c_str();
+            while (psLen > 0)
+            {
+                *(badShift + (unsigned char)*psPtr) = --psLen;
+                cout << (unsigned char)*psPtr << " bad value : "  <<(int )(*(badShift + (unsigned char)*psPtr))  << endl;
+                psPtr++;
+            }
+            
+            cout << endl;
+            for (int i = 0; i < shiftlength; i++)
+            {
+                if ((int)badShift[i]  != (int)ps.length())
+                {
+                    cout << (unsigned char) i << " bad value : "  << (int)badShift[i]  << endl;
+                }
+            }
+            cout << endl;
+        }
+        else
+        {
+            int len = 256;
+            unsigned char *next = new unsigned char[len];
+            
+            buildBadC(ps, next, len);
+            
+            delete [] next;
+        }
+    }
     
+    /*bref 查找出模式串patn在主串src中第一次出现的位置
+    return patn在src中出现的位置，当src中并没有patn时，返回-1*/
+    
+    int bmBadSearchIndexOf(const string macthStr, const string patternStr, unsigned char *shift = nullptr, int shiftLen = 256)
+    {
+        int plen = (int)patternStr.length();
+        if (plen == 0)
+        {
+            return -1;
+        }
+        
+        if (plen > macthStr.length())
+        {
+            return -1;
+        }
+        
+        bool newshift = false;
+        if (shift == nullptr)
+        {
+            newshift =true;
+            shiftLen = 256;
+            shift = new unsigned char[shiftLen];
+        }
+        
+        buildBadC(patternStr, shift, shiftLen);
+        
+        
+        int m_idx = plen, p_idx;
+        int skip;
+        
+        const char *mPtr = macthStr.c_str();
+        const char *pPtr = patternStr.c_str();
+        
+        int count = 0;
+        
+        int mlen = (int) macthStr.length();
+        while (m_idx <= mlen)
+        {
+            p_idx = plen;
+            cout << "第"<< ++count << "次比较： patternCPtr[" << m_idx-1 << "] (" << pPtr[p_idx-1] <<") != matchCPtr[" << m_idx-1 << "] ("<< mPtr[p_idx-1] <<")" << endl;
+            while (mPtr[--m_idx] == pPtr[--p_idx])
+            {
+                if (p_idx != (plen - 1))
+                {
+                    cout << "第"<< ++count << "次比较： patternCPtr[" << m_idx-1 << "] (" << pPtr[p_idx-1] <<") != matchCPtr[" << m_idx-1 << "] ("<< mPtr[p_idx-1] <<")" << endl;
+                }
+                if (p_idx == 0)
+                {
+                    if (newshift)
+                    {
+                        delete [] shift;
+                    }
+                    return m_idx;
+                }
+            }
+            
+            
+            unsigned char m_idx_c = mPtr[m_idx];
+            skip = shift[m_idx_c];
+            m_idx += (skip > (plen - p_idx)) ? skip : plen - p_idx;
+            m_idx += 1;
+            
+        }
+        
+        if (newshift)
+        {
+            delete [] shift;
+        }
+        
+        return -1;
+    }
+    
+    void buildBadCMap(const string ps, map<unsigned char, int> &badMap)
+    {
+        int psLen = (int)ps.length();
+        const char *psPtr = ps.c_str();
+        while (psLen > 0)
+        {
+            unsigned char pc = (unsigned char)(*psPtr);
+            cout << pc << " bad value : "  << psLen - 1  << endl;
+            badMap.erase(pc);
+            badMap.insert(make_pair(pc, --psLen));
+            psPtr++;
+        }
+        
+        cout << endl;
+        std::for_each(badMap.begin(), badMap.end(), [](std::pair<unsigned char, int> n){
+            cout << n.first << " bad value : "  << n.second  << endl;
+        });
+    }
+    
+    int bmBadMapSearchIndexOf(const string macthStr, const string patternStr)
+    {
+        int plen = (int)patternStr.length();
+        if (plen == 0)
+        {
+            return -1;
+        }
+        
+        if (plen > macthStr.length())
+        {
+            return -1;
+        }
+        
+        map<unsigned char, int> badmap;
+        
+        
+        buildBadCMap(patternStr, badmap);
+        
+        
+        int m_idx = plen, p_idx;
+        int skip;
+        
+        const char *mPtr = macthStr.c_str();
+        const char *pPtr = patternStr.c_str();
+        
+        int count = 0;
+        
+        int mlen = (int) macthStr.length();
+        while (m_idx <= mlen)
+        {
+            p_idx = plen;
+            cout << "第"<< ++count << "次比较： patternCPtr[" << m_idx-1 << "] (" << pPtr[p_idx-1] <<") != matchCPtr[" << m_idx-1 << "] ("<< mPtr[p_idx-1] <<")" << endl;
+            while (mPtr[--m_idx] == pPtr[--p_idx])
+            {
+                if (p_idx != (plen - 1))
+                {
+                    cout << "第"<< ++count << "次比较： patternCPtr[" << m_idx-1 << "] (" << pPtr[p_idx-1] <<") != matchCPtr[" << m_idx-1 << "] ("<< mPtr[p_idx-1] <<")" << endl;
+                }
+                if (p_idx == 0)
+                {
+                    return m_idx;
+                }
+            }
+            
+            
+            unsigned char m_idx_c = mPtr[m_idx];
+            skip = badmap.find(m_idx_c) == badmap.end() ? (int)patternStr.length() : badmap.at(m_idx_c);
+            m_idx += (skip > (plen - p_idx)) ? skip : plen - p_idx;
+            m_idx += 1;
+            
+        }
+        
+        return -1;
+    }
+    
+    
+    void buildSundayMap(const string ps, map<unsigned char, int> &badMap)
+    {
+        int psLen = (int)ps.length();
+        const char *psPtr = ps.c_str();
+        int i = 0;
+        while (i < psLen)
+        {
+            unsigned char pc = (unsigned char)(*(psPtr + i));
+            cout << pc << " bad value : "  << i << endl;
+            badMap.erase(pc);
+            badMap.insert(make_pair(pc, i));
+            i++;
+        }
+        
+        cout << endl;
+        std::for_each(badMap.begin(), badMap.end(), [](std::pair<unsigned char, int> n){
+            cout << n.first << " bad value : "  << n.second  << endl;
+        });
+    }
+    
+    int SundaySearch(const string macthStr, const string patternStr)
+    {
+        int plen = (int)patternStr.length();
+        if (plen == 0)
+        {
+            return -1;
+        }
+        
+        if (plen > macthStr.length())
+        {
+            return -1;
+        }
+        
+        map<unsigned char, int> badmap;
+        
+        
+        buildSundayMap(patternStr, badmap);
+        
+        int matcnLen = (int)macthStr.length();
+        int patternLen = (int)patternStr.length();
+        
+        const char *mPtr = macthStr.c_str();
+        const char *pPtr = patternStr.c_str();
+        
+        int count = 0;
+        
+        cout << macthStr << endl;
+        cout << patternStr << endl;
+        for (int nBegin = 0 ; nBegin < matcnLen - patternLen; )
+        {
+            int i = nBegin, j = 0;
+            
+            bool flag = false;
+            if (mPtr[nBegin + plen - 1] == pPtr[plen - 1])
+            {
+            
+                cout << "第"<< ++count << "次比较： patternCPtr[" << plen - 1 << "] (" << pPtr[plen - 1] <<") != matchCPtr[" << nBegin + plen - 1 << "] ("<< mPtr[nBegin + plen - 1] <<")" << endl;
+                for (; j < patternLen - 1 && i < matcnLen && mPtr[i] == pPtr[j]; i++, j++)
+                {
+                    flag = true;
+                    cout << "第"<< ++count << "次比较： patternCPtr[" << j << "] (" << pPtr[j] <<") == matchCPtr[" << i << "] ("<< mPtr[j] <<")" << endl;
+                }
+                
+                if (!flag)
+                {
+                    cout << "第"<< ++count << "次比较： patternCPtr[" << j << "] (" << pPtr[j] <<") != matchCPtr[" << i << "] ("<< mPtr[j] <<")" << endl;
+                }
+            }
+            else
+            {
+                cout << "第"<< ++count << "次比较： patternCPtr[" << plen - 1 << "] (" << pPtr[plen - 1] <<") != matchCPtr[" << nBegin + plen - 1 << "] ("<< mPtr[nBegin + plen - 1] <<")" << endl;
+            }
+            
+            
+            
+            
+            
+            if (j == patternLen)
+            {
+                return nBegin;
+            }
+            
+            if (nBegin + patternLen > matcnLen)
+            {
+                return -1;
+            }
+            else
+            {
+                
+                unsigned char pc = mPtr[nBegin + patternLen];
+                
+                int skip = badmap.find(pc) == badmap.end() ? -1 : badmap.at(pc);
+                
+                nBegin += patternLen - skip;
+                
+                cout << pc << "后移" << patternLen << " - " <<  skip << " = " << patternLen - skip << endl;
+                
+                cout << macthStr << endl;
+                
+                int ai = 0;
+                while (ai++ < nBegin)
+                {
+                    cout << " ";
+                }
+                cout << patternStr << endl;
+            }
+        }
+        
+        return -1;
+    }
     
 };
 
@@ -273,6 +579,25 @@ int main()
     index = ss.kmpSearchIndexOf(matchStr, patternStr);
     cout << "搜索到 : " << index << endl;
     
+    
+    cout << "========BM Bad搜索=======" <<  matchStr <<"  模式串：" << patternStr << endl;
+    index = ss.bmBadSearchIndexOf(matchStr, patternStr);
+    cout << "搜索到 : " << index << endl;
+    
+    cout << "========BM BadMap搜索=======" <<  matchStr <<"  模式串：" << patternStr << endl;
+    index = ss.bmBadMapSearchIndexOf(matchStr, patternStr);
+    cout << "搜索到 : " << index << endl;
+    
+    cout << "========Sunday 搜索=======" <<  matchStr <<"  模式串：" << patternStr << endl;
+    index = ss.SundaySearch(matchStr, patternStr);
+    cout << "搜索到 : " << index << endl;
+    
+    
+    cout << "========BM Bad 表======= :" << patternStr  << endl;
+    ss.buildBadC(patternStr);
+    
+    std::map<unsigned char, int> map;
+    ss.buildBadCMap(patternStr, map);
 //    cout << "========KMPV2搜索=======: 匹配串" <<  "ABCD" <<"  模式串：" << "BC" << endl;
 //    index = ss.kmpSearchIndexOfV2("ABCD", "BC");
 //    cout << "搜索到 : " << index << endl;
